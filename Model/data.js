7ffb01263b8f61b0c40fa4477c7c84ccf07d79cc
soxzen <soxzen6@gmail.com>
2023-06-19 17:24:38 +0200

    install cors

--- Model/data.js
+++ Model/data.js
@@ -1,47 +1,43 @@
 const data = [
     {
         "id":1,
-        "Question":"Quelle est la femelle du hamster ?",
-        "Réponse":"L'Amsterdam."
+        "question":"Quelle est la femelle du hamster ? L'Amsterdam."
     },
     {
         "id":2,
-        "Question":"Que dit un oignon quand il se cogne ?",
-        "Réponse":"Aie."
+        "question":"Que dit un oignon quand il se cogne ? Aie."
     },
     {
         "id":3,
-        "Question":"Quel est l'animal le plus heureux ?",
-        "Réponse":"Le hibou, parce que sa femme est chouette."
+        "question":"Quel est l'animal le plus heureux ? Le hibou, parce que sa femme est chouette."
     },
     {
         "id":4,
-        "Question":"Pourquoi le football c'est rigolo ?",
-        "Réponse":"Parce que Thierry en rit."
+        "question":"Pourquoi le football c'est rigolo ? Parce que Thierry en rit."
     },
     {
         "id":5,
-        "Blagues":"Quel est le sport le plus fuité ? La boxe, parce que tu prends des pêches dans la poire et tu tombes dans les pommes."
+        "question":"Quel est le sport le plus fuité ? La boxe, parce que tu prends des pêches dans la poire et tu tombes dans les pommes."
     },
     {
         "id":6,
-        "Question":"Que se fait un Schtroumpf quand il tombe? un Bleu."
+        "question":"Que se fait un Schtroumpf quand il tombe? un Bleu."
     },
     {
         "id":7,
-        "Question":"Quel  est le comble pour un marin ? Avoir le nez qui coule !"
+        "question":"Quel  est le comble pour un marin ? Avoir le nez qui coule !"
     },
     {
         "id":8,
-        "Question":"Qu'est ce que les enfants usent le plus à l'école ? Le professeur !"
+        "question":"Qu'est ce que les enfants usent le plus à l'école ? Le professeur !"
     },    
     {
         "id":9,
-        "Question":"Quel est le sport le plus silencieux ? Le para-chuuuut !"
+        "question":"Quel est le sport le plus silencieux ? Le para-chuuuut !"
     },
     {
         "id":10,
-        "Question":"Quel est le comble pour un joueur de bowling ? C'est de perdre la boule!"
+        "question":"Quel est le comble pour un joueur de bowling ? C'est de perdre la boule!"
     }
 ]
 
