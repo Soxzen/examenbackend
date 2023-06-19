43c9cdcad4c7c16a03f9ea7c91276d09078f53ca
soxzen <soxzen6@gmail.com>
2023-06-19 16:49:48 +0200

    v2 correction bdd

--- Model/data.js
+++ Model/data.js
@@ -21,33 +21,27 @@ const data = [
     },
     {
         "id":5,
-        "Question":"Quel est le sport le plus fuité ?",
-        "Réponse":"La boxe, parce que tu prends des pêches dans la poire et tu tombes dans les pommes."
+        "Blagues":"Quel est le sport le plus fuité ? La boxe, parce que tu prends des pêches dans la poire et tu tombes dans les pommes."
     },
     {
         "id":6,
-        "Question":"Que se fait un Schtroumpf quand il tombe?",
-        "Réponse":"un Bleu."
+        "Question":"Que se fait un Schtroumpf quand il tombe? un Bleu."
     },
     {
         "id":7,
-        "Question":"Quel  est le comble pour un marin",
-        "Réponse":"Avoir le nez qui coule !"
+        "Question":"Quel  est le comble pour un marin ? Avoir le nez qui coule !"
     },
     {
         "id":8,
-        "Question":"Qu'est ce que les enfants usent le plus à l'école ?",
-        "Réponse":"Le professeur !"
+        "Question":"Qu'est ce que les enfants usent le plus à l'école ? Le professeur !"
     },    
     {
         "id":9,
-        "Question":"Quel est le sport le plus silencieux ?",
-        "Réponse":"Le para-chuuuut !"
+        "Question":"Quel est le sport le plus silencieux ? Le para-chuuuut !"
     },
     {
         "id":10,
-        "Question":"Quel est le comble pour un joueur de bowling ?",
-        "Réponse":"C'est de perdre la boule!"
+        "Question":"Quel est le comble pour un joueur de bowling ? C'est de perdre la boule!"
     }
 ]
 
